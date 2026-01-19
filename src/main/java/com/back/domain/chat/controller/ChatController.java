1d59c8991ea9ed504c692f77c8d229d865c838b7
AquilaXk <illusiveman7@gmail.com>
2026-01-19 15:30:36 +0900

    [Feat/#1] ChatController 구현

---
+++ src/main/java/com/back/domain/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.back.domain.chat.controller;
+
+import com.back.domain.chat.dto.ChatDto;
+import com.back.domain.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody ChatDto chatDto) {
+        chatService.saveMessage(chatDto);
+    }
+
+    @GetMapping("/room/{roomId}")
+    public List<ChatDto> getMessages(@PathVariable String roomId) {
+        return chatService.getMessages(roomId);
+    }
+}
