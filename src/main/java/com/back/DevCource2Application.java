a8a857e1a763fb3fe51bbdd0df8aad3ed3986653
bindoong01 <mailsky9687@gmail.com>
2026-01-19 10:00:39 +0900

    초기 설정1

---
+++ src/main/java/com/back/DevCource2Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCource2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCource2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/DevCource2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCource2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
