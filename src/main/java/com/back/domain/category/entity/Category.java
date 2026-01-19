21cec5c3734c6d58438300b75432d3525fd9b6a7
bindoong01 <mailsky9687@gmail.com>
2026-01-19 14:21:44 +0900

    [Feat/#3] 경매 관련 카테고리 엔티티 클래스 구현

---
+++ src/main/java/com/back/domain/category/entity/Category.java
@@ -0,0 +1,25 @@
+package com.back.domain.category.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
+
