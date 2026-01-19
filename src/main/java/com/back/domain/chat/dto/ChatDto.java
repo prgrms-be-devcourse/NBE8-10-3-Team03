ce19dc8724cc2b4d6b69e561bd48ab59cf160e65
AquilaXk <illusiveman7@gmail.com>
2026-01-19 16:18:46 +0900

    [Refactor/#1] ChatDto 리팩토링

--- src/main/java/com/back/domain/chat/dto/ChatDto.java
+++ src/main/java/com/back/domain/chat/dto/ChatDto.java
@@ -3,10 +3,12 @@ package com.back.domain.chat.dto;
 import java.time.LocalDateTime;
 
 public record ChatDto(
+        int id,
         Long itemId, // 상품 ID
         String roomId,   // 방 번호
         String sender,   // 보낸 사람
         String message,  // 메시지 내용
-        LocalDateTime sendTime // 보낸 시간
+        LocalDateTime createDate, // 보낸 시간
+        boolean isRead
 ) {
 }
--- src/main/java/com/back/domain/chat/entity/Chat.java
+++ src/main/java/com/back/domain/chat/entity/Chat.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
