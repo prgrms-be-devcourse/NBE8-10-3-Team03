cdfb53e55b48fb916fa25de58bb357f00ea2f4c8
AquilaXk <illusiveman7@gmail.com>
2026-01-19 15:18:37 +0900

    [Feat/#1] ChatService 구현

---
+++ src/main/java/com/back/domain/chat/service/ChatService.java
@@ -0,0 +1,40 @@
+package com.back.domain.chat.service;
+
+import com.back.domain.chat.dto.ChatDto;
+import com.back.domain.chat.entity.Chat;
+import com.back.domain.chat.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChatService {
+
+    private final ChatRepository chatRepository;
+
+    @Transactional
+    public void saveMessage(ChatDto chatDto) {
+        Chat chatMessage = Chat.builder()
+                .itemId(chatDto.itemId())
+                .roomId(chatDto.roomId())
+                .sender(chatDto.sender())
+                .message(chatDto.message())
+                .sendTime(LocalDateTime.now())
+                .build();
+
+        chatRepository.save(chatMessage);
+    }
+
+    public List<ChatDto> getMessages(String roomId) {
+        return chatRepository.findAllByRoomIdOrderBySendTimeAsc(roomId)
+                .stream()
+                .map(m -> new ChatDto(m.getItemId(), m.getRoomId(), m.getSender(), m.getMessage(), m.getSendTime()))
+                .collect(Collectors.toList());
+    }
+}
