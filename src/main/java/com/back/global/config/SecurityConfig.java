d3c4120f05d24326caf920484f265ed36f9a6bfd
bindoong01 <mailsky9687@gmail.com>
2026-01-19 14:24:11 +0900

    [Feat/#3] 임시 config file 구현

---
+++ src/main/java/com/back/global/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.back.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 현재는 모든 요청 허용 (JWT 구현 전)
+                );
+
+        return http.build();
+    }
+}
+
