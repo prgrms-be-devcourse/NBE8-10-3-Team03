a268dbde750b09ffd72313743863ff6ea373d74b
chaeun12 <ic366632@gmail.com>
2026-01-20 12:42:04 +0900

    [Feat/#18] PostController API v1 적용 및 중복 호출 버그 수정

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,27 +1,44 @@
 package com.back.domain.post.post.controller;
 
-import com.back.domain.post.post.dto.*;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.post.post.dto.PostDto;
+import com.back.domain.post.post.dto.PostSaveRequest;
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.exception.ServiceException;
+import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+    private final Rq rq;
 
-    @PostMapping
-    public RsData<PostDto> write(@Valid @RequestBody PostSaveRequest req) {
-        return new RsData<>("201-1", "등록 성공", new PostDto(postService.write(null, req.title(), req.content(), req.price())));
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public RsData<PostDto> write(@Valid @ModelAttribute PostSaveRequest request) {
+        Member actor = rq.getActor();
+
+        if (actor == null) {
+            throw new ServiceException("401-1", "로그인 후 이용해주세요.");
+        }
+
+        Post post = postService.write(actor, request);
+
+        return new RsData<>("201-1", "게시글이 등록되었습니다.", new PostDto(post));
     }
+
 
     @PatchMapping("/{id}")
     public RsData<Void> modify(@PathVariable int id, @Valid @RequestBody PostSaveRequest req) {
-        postService.modify(id, req.title(), req.content(), req.price());
+        postService.modify(id, req);
         return new RsData<>("200-1", "수정 성공");
     }
 
