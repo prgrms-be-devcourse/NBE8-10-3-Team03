cb93d2fee9ecee06a7a76e61cb7757ee918d6e62
kimhss <hyunuary108@kyonggi.ac.kr>
2026-01-20 14:30:16 +0900

    [Feat/#21] Reputation 엔티티 신용도 증감 메서드

--- src/main/java/com/back/domain/member/member/entity/Reputation.java
+++ src/main/java/com/back/domain/member/member/entity/Reputation.java
@@ -1,6 +1,7 @@
 package com.back.domain.member.member.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -16,9 +17,27 @@ public class Reputation extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
     private double score;
+    private int caution;
 
     public Reputation(Member member, double score) {
         this.member = member;
         this.score = score;
+        this.caution = 0;
+    }
+
+    public void update(int caution) {
+        this.caution = caution;
+    }
+
+    // 10% 감소
+    public void decrease() {
+        double newScore = this.getScore() * 0.9;
+        this.score = newScore > 0 ? newScore : 0;
+    }
+
+    // 10% 증가
+    public void increase() {
+        double newScore = this.score * 1.1;
+        this.score = Math.min(newScore, 100);
     }
 }
