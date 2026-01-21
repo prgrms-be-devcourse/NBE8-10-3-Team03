262d6fb7a3d087e4a4c7f4dd40aa69acc98db645
AquilaXk <illusiveman7@gmail.com>
2026-01-21 23:33:14 +0900

    [Refactor/#60] 채팅 레포지토리 리팩토링

--- src/main/java/com/back/domain/chat/chat/repository/ChatRepository.java
+++ src/main/java/com/back/domain/chat/chat/repository/ChatRepository.java
@@ -18,9 +18,8 @@ public interface ChatRepository extends JpaRepository<Chat, Integer> {
 
     // 읽음 처리 (JPQL)
     @Modifying(clearAutomatically = true)
-    @Query("UPDATE Chat c SET c.isRead = true WHERE c.chatRoom.roomId = :roomId AND c.sender != :readerName AND c.isRead = false")
-    void markMessagesAsRead(@Param("roomId") String roomId,
-                            @Param("readerName") String readerName);
+    @Query("UPDATE Chat c SET c.isRead = true WHERE c.chatRoom.roomId = :roomId AND c.senderId != :readerId AND c.isRead = false")
+    void markMessagesAsRead(@Param("roomId") String roomId, @Param("readerId") Integer readerId);
 
     // 참여한 대화의 최신 메세지들 (JPQL)
     @Query("SELECT c FROM Chat c " +
@@ -28,4 +27,7 @@ public interface ChatRepository extends JpaRepository<Chat, Integer> {
             "AND ((c.chatRoom.sellerId = :apiKey AND c.chatRoom.sellerExited = false) " +
             "OR (c.chatRoom.buyerId = :apiKey AND c.chatRoom.buyerExited = false))")
     List<Chat> findAllLatestMessagesByMember(@Param("apiKey") String apiKey);
+
+    // 안 읽은 메세지 개수 조회
+    long countByChatRoom_RoomIdAndIsReadFalseAndSenderIdNot(String roomId, Integer readerId);
 }
