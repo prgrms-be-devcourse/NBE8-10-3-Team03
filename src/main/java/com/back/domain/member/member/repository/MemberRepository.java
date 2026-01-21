9924d41adcfe8567f50e17a2f49370cd98d8fd7a
kimhss <hyunuary108@kyonggi.ac.kr>
2026-01-21 22:24:59 +0900

    [Feat/#56] MemberRepository 비활성화 회원 조회

--- src/main/java/com/back/domain/member/member/repository/MemberRepository.java
+++ src/main/java/com/back/domain/member/member/repository/MemberRepository.java
@@ -3,11 +3,15 @@ package com.back.domain.member.member.repository;
 import com.back.domain.member.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByUsername(String username);
 
     Optional<Member> findByApiKey(String apiKey);
+
+    List<Member> findByActiveFalseAndSuspendAtBefore(LocalDateTime time);
 }
 
