513eef59a0af43320a90c0e0113c575047ea6e4f
kimhss <hyunuary108@kyonggi.ac.kr>
2026-01-21 12:45:31 +0900

    [Feat/#45] AuctionCreateRequest에 AllArgsConstructor 추가

--- src/main/java/com/back/domain/auction/auction/dto/request/AuctionCreateRequest.java
+++ src/main/java/com/back/domain/auction/auction/dto/request/AuctionCreateRequest.java
@@ -3,6 +3,7 @@ package com.back.domain.auction.auction.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AuctionCreateRequest {
 
     @NotBlank(message = "물품 이름은 필수입니다.")
