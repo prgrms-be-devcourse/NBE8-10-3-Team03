e953fc3cc7c018f5d4004353fee9cc41d9c812b0
kimhss <hyunuary108@kyonggi.ac.kr>
2026-01-22 15:29:18 +0900

    [Feat/#73] ãMemberStatus 추가

---
+++ src/main/java/com/back/domain/member/member/enums/MemberStatus.java
@@ -0,0 +1,8 @@
+package com.back.domain.member.member.enums;
+
+public enum MemberStatus {
+    ACTIVE,        // 정상
+    SUSPENDED,     // 일시 정지
+    WITHDRAWN,     // 회원 탈퇴
+    BANNED         // 영구 정지
+}
