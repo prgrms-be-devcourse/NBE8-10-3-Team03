9cf1e823b38fd4210b7aa5188669cca0514a0be9
AquilaXk <illusiveman7@gmail.com>
2026-01-28 15:21:57 +0900

    [Refactor/#122] ChatRepository 쿼리 최적화

--- src/main/java/com/back/domain/image/image/repository/ImageRepository.java
+++ src/main/java/com/back/domain/image/image/repository/ImageRepository.java
@@ -2,9 +2,20 @@ package com.back.domain.image.image.repository;
 
 import com.back.domain.image.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+    @Query("SELECT pi.post.id, i.url FROM PostImage pi JOIN pi.image i " +
+            "WHERE pi.post.id IN :ids AND pi.image.id = (SELECT MIN(pi2.image.id) FROM PostImage pi2 WHERE pi2.post.id = pi.post.id)")
+    List<Object[]> findPostMainImages(@Param("ids") List<Integer> ids);
+
+    @Query("SELECT ai.auction.id, i.url FROM AuctionImage ai JOIN ai.image i " +
+            "WHERE ai.auction.id IN :ids AND ai.image.id = (SELECT MIN(ai2.image.id) FROM AuctionImage ai2 WHERE ai2.auction.id = ai.auction.id)")
+    List<Object[]> findAuctionMainImages(@Param("ids") List<Integer> ids);
 }
 
