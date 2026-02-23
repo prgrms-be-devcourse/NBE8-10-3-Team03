e4017f91fc9af07643d32944ed7ef49b21dfaebd
kimhss <hyunuary108@kyonggi.ac.kr>
2026-02-23 21:40:42 +0900

    [Refactor/#37] ReputationRepository 코틀린 migrate

--- src/main/java/com/back/domain/member/reputation/repository/ReputationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.back.domain.member.reputation.repository;
-
-import com.back.domain.member.reputation.entity.Reputation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReputationRepository extends JpaRepository<Reputation, Integer> {
-}
