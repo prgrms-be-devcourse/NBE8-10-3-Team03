491b5e063802de78e982d88e44c697475611dd59
kimhss <hyunuary108@kyonggi.ac.kr>
2026-02-23 21:56:13 +0900

    [Refactor/#35] Reputation 엔티티 코틀린 migrate

--- src/main/java/com/back/domain/member/reputation/entity/Reputation.kt
+++ src/main/java/com/back/domain/member/reputation/entity/Reputation.kt
@@ -8,21 +8,20 @@ import lombok.NoArgsConstructor
 import lombok.Setter
 import kotlin.math.min
 
-@Getter
-@Setter
 @Entity
 @Table(name = "reputation", indexes = [Index(name = "idx_reputation_member", columnList = "member_id", unique = true)])
-@NoArgsConstructor
 class Reputation(
-    @field:JoinColumn(name = "member_id") @field:OneToOne private var member: Member?,
-    private var score: Double
+    @field:JoinColumn(name = "member_id")
+    @field:OneToOne
+    private var member: Member,
+    var score: Double
 ) : BaseEntity() {
-    private var notifyCount = 0 // 신고 누적 (리셋o)
-    private var totalNotifyCount = 0 // 신고 total 누적 (리셋x)
+    var notifyCount = 0 // 신고 누적 (리셋o)
+    var totalNotifyCount = 0 // 신고 total 누적 (리셋x)
 
     // 10% 감소
     fun decrease() {
-        val newScore = this.getScore() * 0.9
+        val newScore = this.score * 0.9
         this.score = if (newScore > 0) newScore else 0.0
     }
 
