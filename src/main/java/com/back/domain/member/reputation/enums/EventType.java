1bc02b52b711c2a16a5568c43bb9c734db43989e
kimhss <hyunuary108@kyonggi.ac.kr>
2026-02-23 21:38:51 +0900

    [Refactor/#37] Reputation Enum 코틀린 migrate

--- src/main/java/com/back/domain/member/reputation/enums/EventType.java
+++
@@ -1,6 +0,0 @@
-package com.back.domain.member.reputation.enums;
-
-public enum EventType {
-    DEAL_DONE,
-    CANCEL
-}
---
+++ src/main/java/com/back/domain/member/reputation/enums/EventType.kt
@@ -0,0 +1,6 @@
+package com.back.domain.member.reputation.enums
+
+enum class EventType {
+    DEAL_DONE,
+    CANCEL
+}
