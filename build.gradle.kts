4b912789f535fe42a5a0acff3b42219d4013a003
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2026-02-24 17:47:03 +0900

    chore ktlint 의존성 추가 ref #89

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("plugin.lombok") version "2.2.20"
     id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
 
 group = "com"
@@ -68,8 +69,8 @@ dependencies {
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 
     // prometheus
-    implementation ("org.springframework.boot:spring-boot-starter-actuator")
-    implementation ("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
     // bucket4j
     implementation("com.bucket4j:bucket4j-core:8.10.1")
