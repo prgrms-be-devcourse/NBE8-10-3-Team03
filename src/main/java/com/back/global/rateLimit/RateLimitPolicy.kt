7dc912a87b765de501c8957c39cfc40a6e65b126
kimhss <hyunuary108@kyonggi.ac.kr>
2026-02-24 10:02:20 +0900

    [Refactor/#42] RateLimitPolicy 코틀린 migrate

--- src/main/java/com/back/global/rateLimit/RateLimitPolicy.kt
+++ src/main/java/com/back/global/rateLimit/RateLimitPolicy.kt
@@ -1,17 +1,12 @@
 package com.back.global.rateLimit
 
-import lombok.AllArgsConstructor
-import lombok.Getter
 import java.time.Duration
 
-@Getter
-@AllArgsConstructor
-enum class RateLimitPolicy {
+enum class RateLimitPolicy(
+    val limit: Int,
+    val duration: Duration
+) {
     LOGIN(5, Duration.ofMinutes(1)),
     API_DEFAULT(100, Duration.ofMinutes(1)),
-    API_HEAVY(20, Duration.ofMinutes(1));
-
-    private val limit = 0
-    private val duration: Duration? = null
+    API_HEAVY(20, Duration.ofMinutes(1))
 }
-
