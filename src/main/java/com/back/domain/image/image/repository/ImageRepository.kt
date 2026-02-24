ee6e8e530ac192d50d7cf71d583ececf681bd0d1
AquilaXk <illusiveman7@gmail.com>
2026-02-24 14:46:40 +0900

    [Refactor/#71] 채팅 로직 캐스팅 안전성 강화

--- src/main/java/com/back/domain/image/image/repository/ImageRepository.kt
+++ src/main/java/com/back/domain/image/image/repository/ImageRepository.kt
@@ -12,11 +12,11 @@ interface ImageRepository : JpaRepository<Image, Int> {
         "SELECT pi.post.id, i.url FROM PostImage pi JOIN pi.image i " +
             "WHERE pi.post.id IN :ids AND pi.image.id = (SELECT MIN(pi2.image.id) FROM PostImage pi2 WHERE pi2.post.id = pi.post.id)"
     )
-    fun findPostMainImages(@Param("ids") ids: List<Int>): List<Array<Any>>
+    fun findPostMainImages(@Param("ids") ids: List<Int>): List<Array<Any?>>
 
     @Query(
         "SELECT ai.auction.id, i.url FROM AuctionImage ai JOIN ai.image i " +
             "WHERE ai.auction.id IN :ids AND ai.image.id = (SELECT MIN(ai2.image.id) FROM AuctionImage ai2 WHERE ai2.auction.id = ai.auction.id)"
     )
-    fun findAuctionMainImages(@Param("ids") ids: List<Int>): List<Array<Any>>
+    fun findAuctionMainImages(@Param("ids") ids: List<Int>): List<Array<Any?>>
 }
