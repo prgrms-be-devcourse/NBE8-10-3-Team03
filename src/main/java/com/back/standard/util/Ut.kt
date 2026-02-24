8778a498648c754e76803f14be231721f01ffc4c
kimhss <hyunuary108@kyonggi.ac.kr>
2026-02-24 14:29:14 +0900

    [Refactor/#64] Ut 클래스 코틀린 migrate

--- src/main/java/com/back/standard/util/Ut.kt
+++ src/main/java/com/back/standard/util/Ut.kt
@@ -8,63 +8,53 @@ import java.util.*
 
 class Ut {
     object jwt {
-        fun toString(secret: String, expireSeconds: Int, body: MutableMap<String?, Any?>): String? {
-            val claimsBuilder = Jwts.claims()
-
-            for (entry in body.entries) {
-                claimsBuilder.add(entry.key, entry.value)
-            }
-
-            val claims = claimsBuilder.build()
+        @JvmStatic
+        fun toString(secret: String, expireSeconds: Int, body: MutableMap<String, Any>): String {
+            val claims = Jwts.claims().apply {
+                body.forEach { (key, value) -> add(key, value) }
+            }.build()
 
             val issuedAt = Date()
             val expiration = Date(issuedAt.getTime() + 1000L * expireSeconds)
-
             val secretKey: Key = Keys.hmacShaKeyFor(secret.toByteArray())
 
-            val jwt = Jwts.builder()
+            return Jwts.builder()
                 .claims(claims)
                 .issuedAt(issuedAt)
                 .expiration(expiration)
                 .signWith(secretKey)
                 .compact()
-
-            return jwt
         }
 
+        @JvmStatic
         fun isValid(secret: String, jwtStr: String?): Boolean {
             val secretKey = Keys.hmacShaKeyFor(secret.toByteArray())
 
-            try {
+            return runCatching {
                 Jwts.parser()
                     .verifyWith(secretKey)
                     .build()
                     .parseSignedClaims(jwtStr)
-            } catch (e: Exception) {
-                return false
-            }
-
-            return true
+            }.isSuccess
         }
 
-        fun payload(secret: String, jwtStr: String?): MutableMap<String?, Any?>? {
+        @JvmStatic
+        fun payload(secret: String, jwtStr: String?): MutableMap<String, Any>? {
             val secretKey = Keys.hmacShaKeyFor(secret.toByteArray())
 
-            try {
-                return LinkedHashMap<String?, Any?>(
-                    Jwts.parser()
-                        .verifyWith(secretKey)
-                        .build()
-                        .parseSignedClaims(jwtStr)
-                        .getPayload()
-                )
-            } catch (e: Exception) {
-                return null
-            }
+            return runCatching {
+                Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(jwtStr)
+                    .payload
+                    .toMap()
+            }.getOrNull() as MutableMap<String, Any>?
         }
     }
 
     object json {
+        @JvmField
         var objectMapper: ObjectMapper? = null
 
         @JvmOverloads
