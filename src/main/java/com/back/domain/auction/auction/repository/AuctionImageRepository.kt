0d756e58baca0835aec37329c614730d063115b7
bindoong01 <mailsky9687@gmail.com>
2026-02-25 10:17:06 +0900

    [refactor/#101] AuctionImageRepository Kotlin 전환

---
+++ src/main/java/com/back/domain/auction/auction/repository/AuctionImageRepository.kt
@@ -0,0 +1,9 @@
+package com.back.domain.auction.auction.repository
+
+import com.back.domain.auction.auction.entity.AuctionImage
+import com.back.domain.auction.auction.entity.AuctionImageId
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AuctionImageRepository : JpaRepository<AuctionImage, AuctionImageId>
