193d82ef811646c92f26df0eee76ad1036f6097d
AquilaXk <illusiveman7@gmail.com>
2026-02-25 09:26:46 +0900

    [Refactor/#88]] ChatRoomRepository 코드 코틀린 스럽게 리팩토링

--- src/main/java/com/back/domain/chat/chat/repository/ChatRoomRepository.kt
+++ src/main/java/com/back/domain/chat/chat/repository/ChatRoomRepository.kt
@@ -4,15 +4,14 @@ import com.back.domain.auction.auction.entity.Auction
 import com.back.domain.chat.chat.entity.ChatRoom
 import com.back.domain.post.post.entity.Post
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface ChatRoomRepository : JpaRepository<ChatRoom, Int> {
     // UUID로 방 찾기 (삭제되지 않은 방만)
-    fun findByRoomIdAndDeletedFalse(roomId: String): Optional<ChatRoom>
+    fun findByRoomIdAndDeletedFalse(roomId: String): ChatRoom?
 
     // POST: 이미 존재하는 방인지 확인 (삭제되지 않은 방만)
-    fun findByPostAndBuyerApiKeyAndDeletedFalse(post: Post, buyerId: String): Optional<ChatRoom>
+    fun findByPostAndBuyerApiKeyAndDeletedFalse(post: Post, buyerApiKey: String): ChatRoom?
 
     // Auction: 이미 존재하는 방인지 확인 (삭제되지 않은 방만)
-    fun findByAuctionAndBuyerApiKeyAndDeletedFalse(auction: Auction, buyerId: String): Optional<ChatRoom>
+    fun findByAuctionAndBuyerApiKeyAndDeletedFalse(auction: Auction, buyerApiKey: String): ChatRoom?
 }
