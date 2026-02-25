21793cad2e7cb272aee4c788a47bba19ffaf228f
AquilaXk <illusiveman7@gmail.com>
2026-02-25 14:22:33 +0900

    [Refactor/#106] ChatService 포트 주입으로 변경

--- src/main/java/com/back/domain/chat/chat/repository/ChatRoomRepository.kt
+++ src/main/java/com/back/domain/chat/chat/repository/ChatRoomRepository.kt
@@ -1,17 +1,17 @@
 package com.back.domain.chat.chat.repository
 
-import com.back.domain.auction.auction.entity.Auction
 import com.back.domain.chat.chat.entity.ChatRoom
-import com.back.domain.post.post.entity.Post
+import com.back.domain.chat.chat.entity.ChatRoomType
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ChatRoomRepository : JpaRepository<ChatRoom, Int> {
     // UUID로 방 찾기 (삭제되지 않은 방만)
     fun findByRoomIdAndDeletedFalse(roomId: String): ChatRoom?
 
-    // POST: 이미 존재하는 방인지 확인 (삭제되지 않은 방만)
-    fun findByPostAndBuyerApiKeyAndDeletedFalse(post: Post, buyerApiKey: String): ChatRoom?
-
-    // Auction: 이미 존재하는 방인지 확인 (삭제되지 않은 방만)
-    fun findByAuctionAndBuyerApiKeyAndDeletedFalse(auction: Auction, buyerApiKey: String): ChatRoom?
+    // txType + itemId + buyer 기준으로 기존 방 조회
+    fun findByTxTypeAndItemIdAndBuyerApiKeyAndDeletedFalse(
+        txType: ChatRoomType,
+        itemId: Int,
+        buyerApiKey: String,
+    ): ChatRoom?
 }
