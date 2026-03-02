1599f3818ab6a8763cbe500bef882a13e8b65946
kimhss <hyunuary108@kyonggi.ac.kr>
2026-03-02 17:08:31 +0900

    [Refactor/#238] Auction thumbnailUrl 관련 추가

--- src/main/java/com/back/domain/auction/auction/service/port/AuctionImagePort.kt
+++ src/main/java/com/back/domain/auction/auction/service/port/AuctionImagePort.kt
@@ -1,9 +1,10 @@
 package com.back.domain.auction.auction.service.port
 
 import com.back.domain.auction.auction.entity.Auction
+import com.back.domain.auction.auction.entity.AuctionImage
 import org.springframework.web.multipart.MultipartFile
 
 interface AuctionImagePort {
-    fun saveImages(auction: Auction, imageFiles: List<MultipartFile>)
+    fun saveImages(auction: Auction, imageFiles: List<MultipartFile>) : List<AuctionImage>
     fun replaceImages(auction: Auction, keepImageUrls: List<String>?, newImageFiles: List<MultipartFile>?)
 }
